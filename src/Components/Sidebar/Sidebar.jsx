1fb8c7bead6e14dcb0f1113c5c6b5e6a8a4f5e74
sammotaroki <66356790+sammotaroki@users.noreply.github.com>
2024-04-05 13:39:43 +0300

    Updated Sidebar component

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,78 +1,88 @@
 import React from 'react'
-// import { Link } from 'react-router-dom'
 import './Sidebar.css'
-import { IoBookmarkOutline, IoSearch, IoBriefcaseOutline, IoUmbrellaOutline, IoLockOpenOutline } from "react-icons/io5";
-import { GiAfrica, GiPoliceBadge } from "react-icons/gi";
-import { FiBook } from "react-icons/fi";
-import { FaRegListAlt } from "react-icons/fa";
-import { RiStackLine } from "react-icons/ri";
-import { BiTransfer } from "react-icons/bi";
-import { TbAlertSquareRounded } from "react-icons/tb";
-import { MdOutlineComputer, MdOutlineRemoveRedEye } from "react-icons/md";
-import { TiContacts } from "react-icons/ti";
+import { IoSearch, IoUmbrellaSharp } from "react-icons/io5";
+import { BsStack } from "react-icons/bs";
+import { FaRectangleList } from "react-icons/fa6";
+import { GiAfrica, GiSuitcase } from "react-icons/gi";
+import { FaBookOpen, FaBookmark } from "react-icons/fa";
+import { AiFillAlert } from "react-icons/ai";
+import { BiTransferAlt } from "react-icons/bi";
+import { RiChatPrivateFill } from "react-icons/ri";
+import { MdContacts, MdLaptopMac, MdLocalPolice, MdOutlineSecurity } from "react-icons/md";
 import { Link } from 'react-router-dom';
+
+const handleLinkClick = (e) => {
+    e.preventDefault(); // prevent the default action
+    e.stopPropagation(); // stop the click from bubbling
+
+    const selectedList = e.target.closest('div');
+    const selectedLink = e.target;
+    const allLinks = selectedList.querySelectorAll('a');
+    allLinks.forEach((link) => link.classList.remove('selected'));
+    selectedLink.classList.add('selected');
+};
 
 const Sidebar = () => {
     return (
         <div className='sidenav'>
-            <div>
-                <IoBookmarkOutline className='icons' />
-                <Link to="/about" className='links'>About</Link>
-            </div>
-            <div>
+            <div className='side_item selected'>
                 <GiAfrica className='icons' />
                 <Link to="/" className='links'>Africa Map</Link>
             </div>
-            <div>
+            <div className='side_item'>
+                <FaBookmark className='icons' />
+                <Link to="/about" className='links'>About</Link>
+            </div>
+            <div onClick={handleLinkClick}>
                 <IoSearch className='icons' />
                 <Link to="/law" className='links'>Law</Link>
             </div>
             <div>
-                <FiBook className='icons' />
+                <FaBookOpen className='icons' />
                 <Link to="/definition" className='links'>Definition</Link>
             </div>
             <div>
-                <IoBriefcaseOutline className='icons' />
+                <GiSuitcase className='icons' />
                 <Link to="/authority" className='links'>Authority</Link>
             </div>
             <div>
-                <FaRegListAlt className='icons' />
+                <FaRectangleList className='icons' />
                 <Link to="/regulation" className='links'>Regulation</Link>
             </div>
             <div>
-                <IoUmbrellaOutline className='icons' />
+                <IoUmbrellaSharp className='icons' />
                 <Link to="/protectionofficers" className='links'>Data Protection Officers</Link>
             </div>
             <div>
-                <RiStackLine className='icons' />
+                <BsStack className='icons' />
                 <Link to="/collection" className='links'>Collection & Processing</Link>
             </div>
             <div>
-                <BiTransfer className='icons' />
+                <BiTransferAlt className='icons' />
                 <Link to="/transfer" className='links'>Transfer</Link>
             </div>
             <div>
-                <IoLockOpenOutline className='icons' />
+                <MdOutlineSecurity className='icons' />
                 <Link to="/security" className='links'>Security</Link>
             </div>
             <div>
-                <TbAlertSquareRounded className='icons' />
+                <AiFillAlert className='icons' />
                 <Link to="/breach" className='links'>Breach Notification</Link>
             </div>
             <div>
-                <GiPoliceBadge className='icons' />
+                <MdLocalPolice className='icons' />
                 <Link to="/enforcement" className='links'>Enforcement</Link>
             </div>
             <div>
-                <MdOutlineComputer className='icons' />
+                <MdLaptopMac className='icons' />
                 <Link to="/marketing" className='links'>Electronic Marketing</Link>
             </div>
             <div>
-                <MdOutlineRemoveRedEye className='icons' />
+                <RiChatPrivateFill className='icons' />
                 <Link to="/privacy" className='links'>Online Privacy</Link>
             </div>
             <div>
-                <TiContacts className='icons' />
+                <MdContacts className='icons' />
                 <Link to="/contacts" className='links'>Contacts</Link>
             </div>
         </div>
