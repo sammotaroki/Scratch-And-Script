881f3a3446e02dc8ea1f2f425ee65a39f8ddcf45
sammotaroki <66356790+sammotaroki@users.noreply.github.com>
2024-04-05 13:47:16 +0300

    Updated banner component

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Banner.css'
 import logo from '../../assets/snscript.svg'
-import { FaArrowRightLong } from "react-icons/fa6";
+
 
 
 const Banner = () => {
@@ -15,10 +15,6 @@ const Banner = () => {
                 <p>Scratch and Script was developed  <span className='green'> </span> to <span className='highlight'> raise awareness about data protection </span>in Africa and motivate for policy and regulatory change that better protects the right to privacy.</p>
                 <div className='lastUpdate'>
                     Last updated: <span className='period'>February 2024</span>
-                </div>
-                <div className='readme'>
-                    <FaArrowRightLong className='arrowIcon' />
-                    <a href='/'>READ MORE</a>
                 </div>
             </div>
         </div>
